a492d967d40ea288f563ae23c3cd5301b16f83cd
wh1tepr1nc3 <amamamania@mail.ru>
2024-07-28 15:30:13 +0500

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -4,18 +4,13 @@ def get_matrix(n, m, value):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-    print(matrix)
     return matrix
 
-n = 4
-m = 5
-value = 12
-matrix = get_matrix(n, m, value)
 
-if n <= 0:
-    print('Неверное значение строк')
-elif m <=0:
-    print('Неверное значение столбцов')
-# else:          2
-#     for i in matrix:
-#         print(*i)
+result_1 = get_matrix(n=4, m=2, value=12)
+result_2 = get_matrix(n=5, m=9, value=4)
+result_3 = get_matrix(n=8, m=7, value=6)
+
+print(result_1)
+print(result_2)
+print(result_3)
